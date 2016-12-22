e3be1efc39fe9b2767c0aa54136c67cb5617175f
Sean Lee <seanboramlee@gmail.com>
2016-12-21 17:38:42 -0800

    Pomodor is now IndexRoute

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import Pomodoro from './components/Pomodoro'
 import Navigation from './components/Navigation'
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Navigation}>
+    <IndexRoute component={Pomodoro}/>
       <Route path="/pomodoro" component={Pomodoro} />
     </Route>
   </Router>, document.getElementById('app'));
