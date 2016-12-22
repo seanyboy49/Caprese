d96622c3f7b661488fd59ea0537474a6286561f5
Sean Lee <seanboramlee@gmail.com>
2016-12-22 15:36:06 -0800

    add Logs and Tags components to navigation

---
+++ src/components/Tags.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Tags extends React.Component{
+  render() {
+    return (
+      <div>
+        <h1>Tags</h1>
+      </div>
+    )
+  }
+}
+
+export default Tags
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import Pomodoro from './components/Pomodoro'
 import Navigation from './components/Navigation'
+import Tags from './components/Tags'
+import Logs from './components/Logs'
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Navigation}>
     <IndexRoute component={Pomodoro}/>
       <Route path="/pomodoro" component={Pomodoro} />
+      <Route path="/tags" component={Tags} />
+      <Route path="/logs" component={Tags} />
     </Route>
   </Router>, document.getElementById('app'));
