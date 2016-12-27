e2d5a909b5f2b7ff744c27fa79b941f70a40e352
Sean Lee <seanboramlee@gmail.com>
2016-12-27 15:54:36 -0800

    add search form

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import '../styles/SearchForm.css'
+
 
 function puke (object) {
   return <pre>{JSON.stringify(object, null, ' ')}</pre>
+}
+
+function displayMatches(e) {
+  console.log(e)
 }
 
 class Tags extends React.Component{
@@ -15,11 +21,17 @@ class Tags extends React.Component{
     const tags = []
     fetch('http://localhost:3001/tags').then(blob => blob.json()).then(data => this.setState({tags: data}))
   }
+  componentDidMount() {
+    const searchInput = document.querySelector('.search-form')
+    searchInput.addEventListener('keyup', displayMatches)
+  }
   render() {
     return (
       <div>
         <h1>Tags</h1>
-        {puke(this.state.tags)}
+        <form className="search-form">
+          <input type="text" placeholder="Search for a tag"/>
+        </form>
       </div>
     )
   }
