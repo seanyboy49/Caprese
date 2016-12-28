492f5920b9c65d3cc9e262a93798d7b83a530e2f
Sean Lee <seanboramlee@gmail.com>
2016-12-27 17:01:05 -0800

    search input returns target value and tags array

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,37 +1,38 @@
 import React from 'react';
 import '../styles/SearchForm.css'
 
-
-function puke (object) {
-  return <pre>{JSON.stringify(object, null, ' ')}</pre>
-}
-
-function displayMatches(e) {
-  console.log(e)
-}
-
 class Tags extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
       tags: null
     }
+    this.displayMatches = this.displayMatches.bind(this);
+    this.findMatches = this.findMatches.bind(this);
+  }
+  findMatches(wordToMatch, tags) {
+    console.log(wordToMatch, tags)
+  }
+
+  displayMatches(e) {
+    this.findMatches(e.target.value, this.state.tags)
   }
   componentWillMount() {
     const tags = []
-    fetch('http://localhost:3001/tags').then(blob => blob.json()).then(data => this.setState({tags: data}))
+    fetch('http://localhost:3001/tags').then(blob => blob.json()).then(data => this.setState({tags: data.data}))
   }
   componentDidMount() {
-    const searchInput = document.querySelector('.search-form')
-    searchInput.addEventListener('keyup', displayMatches)
+    const searchInput = document.querySelector('.search')
+    searchInput.addEventListener('keyup', this.displayMatches)
   }
   render() {
     return (
       <div>
         <h1>Tags</h1>
         <form className="search-form">
-          <input type="text" placeholder="Search for a tag"/>
+          <input type="text" className="search" placeholder="Search for a tag"/>
         </form>
+
       </div>
     )
   }
